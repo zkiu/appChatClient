482de8252e726051689f8c21b793e2fc6807a6ec
Kiu <zqdummy@gmail.com>
2020-05-05 23:06:06 -0700

    initial commitThis is a working version using just Express.js and minimal JQueryNeed to implement push notification from Socket.IONeed to implement a database

---
+++ script.js
@@ -0,0 +1,34 @@
+// Outputs messages to App --------------------------------------
+
+function AddMessage(message) {
+    const MESSAGE = document.querySelector('#messages')
+    MESSAGE.insertAdjacentHTML('beforeend',`<h4>${message.name}</h4><p>${message.message}</p>`)
+
+}
+
+function GetMessages() {
+    //still have to use JQuery for this. will use plain vanilla JS in the future.
+    $.get('http://localhost:3000/messages', data => {
+        data.forEach(AddMessage);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('loaded')
+    // const message = { name:"kiu", message:"here"}
+    GetMessages(message);
+})
+
+// Caputure submitted messages --------------------------------------
+
+document.querySelector('button').addEventListener('click', () => {
+    let name = document.querySelector('#name').value;
+    let message = document.querySelector('#message').value;
+
+    // alternative, but inefficient way to compose the JSON object
+    // let data = { name: `${name}`, message: `${message}` }
+    // console.log(data);
+    
+    let data = { name: name, message: message }
+    $.post('http://localhost:3000/messages', data)
+})
