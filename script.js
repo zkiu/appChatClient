d52d5670c720c35eb89535f559c2eb30935d9dcf
Kiu <zqdummy@gmail.com>
2020-05-09 20:56:31 -0700

    make comments more pronounce

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// Outputs messages to App --------------------------------------
+// -- Outputs messages to App --------------------------------------
 
-// Append 1 message to html code at id:messages
+// -- Append 1 message to html code at id:messages
 function AddMessage(message) {
     const MESSAGE = document.querySelector('#messages')
     MESSAGE.insertAdjacentHTML('beforeend',`<h4>${message.name}</h4><p>${message.message}</p>`)
 }
 
-// Send GET request for /messages page that contains all the database messages.
+// -- Send GET request for /messages page that contains all the database messages.
 function GetMessages() {
-    //still have to use JQuery for this. will use plain vanilla JS in the future.
-    // port 3000 is explicitly set -> may need to make this a variable in the future in case the port # change dynamically
+    //-- still have to use JQuery for this. will use plain vanilla JS in the future.
+    // -- port 3000 is explicitly set -> may need to make this a variable in the future in case the port # change dynamically
     $.get('http://localhost:3000/messages', data => {
         data.forEach(AddMessage);
     })
 }
 
-// Once app loaded (but before images are loaded), display all the chat history below the chat area 
-// document.addEventListener("DOMContentLoaded", () => {
-//     // console.log('loaded')
-//     GetMessages(message);
-// })
+// -- Once app loaded (but before images are loaded), display all the chat history below the chat area 
+document.addEventListener("DOMContentLoaded", () => {
+    // -- console.log('loaded')
+    GetMessages(message);
+})
 
-// Socket.IO Initialization on client side--------------------------------------
+// -- Socket.IO Initialization on client side--------------------------------------
 var socket = io();
 
-// On btn submit
+// -- On btn submit
 document.querySelector('button').addEventListener('click', () => {
-    // grab text input values from the form
+    // -- grab text input values from the form
     let name = document.querySelector('#name').value;
     let message = document.querySelector('#message').value;
 
-    //create an object with input values and send it to server side
+    // -- create an object with input values and send it to server side
     let data = { name: name, message: message }
-    //pass the form imput to the server
+    // -- pass the form imput to the server
     socket.emit('message', data)
 
     AddMessage(data)
-    //clears the form
+    // -- clears the form
     document.querySelector('#form1').reset()
 })
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-// Typical port used for dev
-// const port = 3000
+// -- Typical port used for dev
+//  -- const port = 3000
 
-// Setting the port requirement for Heroku
+//  -- Setting the port requirement for Heroku
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
@@ -15,19 +15,19 @@ if (port == null || port == "") {
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-//Adding mongoose -----------------------------------------------
+// -- Adding mongoose -----------------------------------------------
 const mongoose = require('mongoose')
-//in production, this url would be in a hidden config file
+// -- in production, this url would be in a hidden config file
 const urlDB = "mongodb+srv://user:user@learningmodulus-yrmc9.mongodb.net/test?retryWrites=true&w=majority" 
 mongoose.connect(urlDB, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// tell mongoose to use the ES6 Promise library
+//  -- tell mongoose to use the ES6 Promise library
 mongoose.Promise = Promise
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=>{
-    // Once connection opens, this callback will be called
+    //  -- Once connection opens, this callback will be called
     console.log('mongoose is connected to MongoDB')
 })
 
@@ -39,13 +39,13 @@ var MsgSchema = {
 
 var Message = mongoose.model('Message', MsgSchema)
 
-//the html file at the root dir is displayed 
+// -- the html file at the root dir is displayed 
 app.use(express.static('.'))
 
-//variable server is only used in the http.listen callback() to get the port number
+// -- variable server is only used in the http.listen callback() to get the port number
 const server = http.listen(port, () => console.log(`Server is listening at http://localhost:${server.address().port}`))
 
-//Respond to the GET /messages page request from the client
+// -- Respond to the GET /messages page request from the client
 app.get('/messages', (req, res) => {
     Message.find({}, (err, messages) => {
         if (err) {
@@ -59,19 +59,19 @@ app.get('/messages', (req, res) => {
 io.on('connection', (socket) => {
     console.log('new client connection');
     
-    //actions on client disconnect
+    // -- actions on client disconnect
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     
     socket.on('message', (message) => {
-        //create a new 'message' document from the Message model/collection
+        // -- create a new 'message' document from the Message model/collection
         let messageReceived = new Message(message)
         messageReceived.save()
             .then(() => {
                 console.log('message saved to MongoDB')
 
-            //  SIMPLE IMPLEMENTATION OF A 'BADWORD FILTER' ---------------------------------
+            //   -- SIMPLE IMPLEMENTATION OF A 'BADWORD FILTER' ---------------------------------
             //     return Message.findOne({ message: 'badword' })
             // })
             // .then((censoredDocument) => {
