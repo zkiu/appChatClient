2110774d341fbb6062df477878c667bad7c80c08
Kiu <zqdummy@gmail.com>
2020-12-19 18:04:03 -0800

    Change mongodb user credentials and indent file

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ const app = express()
 // process.env.NODE_ENV
 // -- "development"
 
-
-
 //  -- Setting the port requirement for Heroku
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
+let port = process.env.PORT
+if (port == null || port == '') {
+	port = 3000
 }
-
-
 
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
@@ -21,33 +17,34 @@ const io = require('socket.io')(http)
 // -- Adding mongoose -----------------------------------------------
 const mongoose = require('mongoose')
 // -- in production, this url would be in a hidden config file
-const urlDB = "mongodb+srv://user:user@learningmodulus-yrmc9.mongodb.net/test?retryWrites=true&w=majority" 
-mongoose.connect(urlDB, { useNewUrlParser: true, useUnifiedTopology: true })
+const urlDB =
+	'mongodb+srv://testUser:9Q4XDruS1nj43UpQ@learncluster.yrmc9.mongodb.net/chatclient?retryWrites=true&w=majority'
+mongoose.connect(urlDB, {useNewUrlParser: true, useUnifiedTopology: true})
 
 //  -- tell mongoose to use the ES6 Promise library
 mongoose.Promise = Promise
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', ()=>{
-    //  -- Once connection opens, this callback will be called
-    console.log('mongoose is connected to MongoDB')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+	//  -- Once connection opens, this callback will be called
+	console.log('mongoose is connected to MongoDB')
 })
 
-//  --defining database structure
+//  --defining mongoose schema
 var MsgSchema = {
-    name: String,
-    message: String,
-    date: { type: Date, default: Date.now }
+	name: String,
+	message: String,
+	date: {type: Date, default: Date.now},
 }
 
 var Message = mongoose.model('Message', MsgSchema)
 
 // -- Middleware to parse the incoming POST requests
-app.use(express.urlencoded({ extended: true })) // -- not used yet in this app
+app.use(express.urlencoded({extended: true})) // -- not used yet in this app
 app.use(express.text())
 
-// -- the html file at the root dir is displayed 
+// -- the html file at the root dir is displayed
 app.use(express.static('.'))
 
 // -- variable server is only used in the http.listen callback() to get the port number
@@ -55,57 +52,50 @@ const server = http.listen(port)
 
 // -- Respond to the GET /messages page request from the client
 app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        if (err) {
-            throw err
-        }
-        res.send(messages)
-    })
+	Message.find({}, (err, messages) => {
+		if (err) {
+			throw err
+		}
+		res.send(messages)
+	})
 })
-
+// -- saving a message
 app.post('/messages', async (req, res) => {
-    try {
-        var message = new Message(JSON.parse(req.body))
-        var savedMessage = await message.save()
-        // console.log(savedMessage.id, ' ', savedMessage.name);
-        
-        console.log('message saved to MongoDB')
+	try {
+		var message = new Message(JSON.parse(req.body))
+		var savedMessage = await message.save()
+		// -- potential filtering code - could be replaced by API
+		// var censored = await Message.findOne({ message: 'badword' })
+		// if (censored)
+		//     await Message.remove({ _id: censored.id })
+		// else
+		// io.emit('message', req.body)
 
-        // -- potential filtering code - could be replaced by API
-        // var censored = await Message.findOne({ message: 'badword' })
-        // if (censored)
-        //     await Message.remove({ _id: censored.id })
-        // else
-            // io.emit('message', req.body)
-        
-        
-        io.emit('appendMessage', savedMessage)
-        res.sendStatus(200)
-
-    } catch (error) {
-        res.sendStatus(500)
-        return console.error(error)
-    } finally {
-        console.log('message post called')
-    }
+		io.emit('appendMessage', savedMessage)
+		res.sendStatus(200)
+	} catch (error) {
+		res.sendStatus(500)
+		return console.error(error)
+	} finally {
+		console.log('Post Request: message saved to MongoDB')
+	}
 })
-
 
 io.on('connection', (socket) => {
-    console.log('new client connection');
-    
-    // -- actions on client disconnect
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+	console.log('new client connection')
 
-    // -- Delete all collections in database
-    socket.on('deleteAll', (deletefunction) => {
-        deletefunction()
-        Message.deleteMany({}, (err) => {
-            if (err) {
-                throw err
-            }
-        })
-    })
+	// -- actions on client disconnect
+	socket.on('disconnect', () => {
+		console.log('user disconnected')
+	})
+
+	// -- Delete all collections in database
+	socket.on('deleteAll', (deletefunction) => {
+		deletefunction()
+		Message.deleteMany({}, (err) => {
+			if (err) {
+				throw err
+			}
+		})
+	})
 })
